ad318d3c3b1f62d0c708d95054126cf6f301d2ec
Mohamed Mourrane <mourranemohamed2020@gmail.com>
2026-04-18 16:42:23 +0100

    changing text

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World ! CI/CD Demo - Version 1.1');
+  res.send('Hello World ! CI/CD Demo - Version 1.2');
 });
 
 app.get('/health', (req, res) => {
