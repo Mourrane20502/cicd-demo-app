0eb73cc2f44d8493e448f77b3fd6b873d5689f47
Mohamed Mourrane <mourranemohamed2020@gmail.com>
2026-04-18 15:41:44 +0100

    new

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World ! CI/CD Demo - Version 1.0');
+  res.send('Hello World ! CI/CD Demo - Version 1.1');
 });
 
 app.get('/health', (req, res) => {
